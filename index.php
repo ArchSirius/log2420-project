eba5b0069e72d617c71b06cd0718503925d89686
Louis Racicot <info@louisracicot.com>
2015-02-25 20:33:54 -0500

    Add comments, footer and stuff

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ require_once __DIR__.'/vendor/autoload.php';
 
 // Instanciate the application
 $app = new Silex\Application();
+
+/**
+ * Configure the app
+ */
+
+// Debug
 $app['debug'] = true;
 
 // Registering the template system Twig
@@ -13,6 +19,11 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 // Register the URL Generator
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+
+/**
+ * Here are the routes
+ */
 
 // Home page
 $app->get('/', function () use ($app) {
@@ -25,5 +36,10 @@ $app->get('/journals', function () use ($app) {
     return $app['twig']->render('journals.twig');
 })
 ->bind('journals');
+
+
+/**
+ * Run the application
+ */
 
 $app->run();
